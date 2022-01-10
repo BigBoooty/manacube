e74b5423a1368092483b8fe45d6f302e4f43cce1
BigBoooty <97455616+BigBoooty@users.noreply.github.com>
2022-01-10 14:27:26 +0000

    Add files via upload

---
+++ manacube-MCC/Start.bat
@@ -0,0 +1,14 @@
+start /min MinecraftClient.exe USERNAME1  PASSWORD1 play.manacube.com
+timeout 2
+
+start /min MinecraftClient.exe USERNAME2  PASSWORD2 play.manacube.com
+timeout 2
+
+start /min MinecraftClient.exe USERNAME3  PASSWORD3 play.manacube.com
+timeout 2
+
+start /min MinecraftClient.exe USERNAME4  PASSWORD4 play.manacube.com
+timeout 2
+
+start /min MinecraftClient.exe USERNAME4  PASSWORD4 play.manacube.com
+timeout 2
